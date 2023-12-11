6e7c14e3c0c9d14c9e970c0345934b6ff4d6638d
Jerfferson-Rodrigues <67125687+Jerfferson-Rodrigues@users.noreply.github.com>
2023-12-11 18:08:19 -0300

    add page login

---
+++ login/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import AppRoutes from "./AppRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
